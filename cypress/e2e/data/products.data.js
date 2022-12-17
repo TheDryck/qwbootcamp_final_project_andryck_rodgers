4071a7fbd1f0f1f733b0b49e5e33ea239248b8d0
Andryck Roders <andryck.roders@jcfdev.local>
2022-12-17 00:05:04 -0500

    Added test cases to assert different ways of sorting products

--- cypress/e2e/data/products.data.js
+++ cypress/e2e/data/products.data.js
@@ -1,98 +1,128 @@
 module.exports = {
     sort: {
-        'A to Z':'az',
-        'Z to A':'za',
-        'Low to High':'lohi',
-        'High to Low':'hilo',
+        'A to Z':'aToZ',
+        'Z to A':'zToA',
+        'Low to High':'lowToHigh',
+        'High to Low':'highToLow',
+    },
+    category: {
+        'Shirts':'shirt',
+        'Pants':'pant',
+        'Hats':'hat',
+        'Shoes':'shoes',
+        'Couch/Sofa':'couch',
+        'Laptops':'laptop'
     },
     products: [
         {
             name: 'Quality Fitted Hat',
-            price: '30'
+            price: '30',
+            //category: 'hat'
         },
         {
             name: 'Quality Trucker Hat',
-            price: '24'
+            price: '24',
+            //category: 'hat'
         },
         {
             name: 'Quality Mousepad',
-            price: '20'
+            price: '20',
+            //category: 'computer'
         },
         {
             name: 'Quality Mug',
-            price: '15'
+            price: '15',
+            //category: 'cup'
         },
         {
             name: 'Quality Pillow',
-            price: '20'
+            price: '20',
+            //category: 'pillow'
         },
         {
             name: 'Quality Hooded Sweatshirt',
-            price: '50'
+            price: '50',
+            //category: 'shirt'
         },
         {
             name: 'Quality Sweatshirt',
-            price: '40'
+            price: '40',
+            //category: 'shirt'
         },
         {
             name: 'Quality Kids Tshirt',
-            price: '15'
+            price: '15',
+            //category: 'shirt'
         },
         {
             name: 'Quality Tshirt',
-            price: '20'
+            price: '20',
+            //category: 'shirt'
         },
         {
             name: 'Quality Vneck',
-            price: '20'
+            price: '20',
+            //category: 'shirt'
         },
         {
             name: 'Quality Pink Pants',
-            price: '44'
+            price: '44',
+            //category: 'pants'
         },
         {
             name: 'Quality Cargo Pants',
-            price: '17'
+            price: '17',
+            //category: 'pants'
         },
         {
             name: 'Quality Jeans Pants',
-            price: '28'
+            price: '28',
+            //category: 'pants'
         },
         {
             name: 'Quality Blue Shoes',
-            price: '20'
+            price: '20',
+            //category: 'shoes'
         },
         {
             name: 'Quality Stylish Shoes',
-            price: '20'
+            price: '20',
+            //category: 'shoes'
         },
         {
             name: 'Quality Heal Shoes',
-            price: '20'
+            price: '20',
+            //category: 'shoes'
         },
         {
             name: 'Red Couch',
-            price: '320'
+            price: '320',
+            //category: 'couch'
         },
         {
             name: 'White Couch',
-            price: '234'
+            price: '234',
+            //category: 'couch'
         },
         {
             name: 'Gray Couch',
-            price: '550'
+            price: '550',
+            //category: 'couch'
         },
         {
             name: 'Mackbook Pro',
-            price: '2000'
+            price: '2000',
+            //category: 'laptop'
         },
         {
             name: 'Dell Laptop',
-            price: '1000'
+            price: '1000',
+            //category: 'laptop'
         },
         {
             name: 'HP Laptop',
-            price: '680'
+            price: '680',
+            //category: 'laptop'
         }
     ]
 }
