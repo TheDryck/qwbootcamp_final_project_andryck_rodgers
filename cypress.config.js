94947412305b0f98d0fb703f1e7bf1023c072c03
Andryck Roders <andryck.roders@jcfdev.local>
2022-12-17 05:05:49 -0500

    Added mocha and updated Test Document with rationale

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,14 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: false,
+      json: true
+    },
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 10000,
   },
-  chromeWebSecurity: false,
-  defaultCommandTimeout: 10000
 });
